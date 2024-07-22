58310859065a5ea4f6edc39b0163409509b410c2
Suhail Ahmed <92672136+Suhail2701@users.noreply.github.com>
2024-07-22 15:50:30 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,41 +1,39 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 
     const registrationForm = document.querySelector('#registrationForm');
-    
-    registrationForm.addEventListener('submit', function(e){
+
+    registrationForm.addEventListener('submit', function (e) {
         e.preventDefault();
 
         const customerIdE = document.getElementById('CustID').value;
         const password = document.getElementById('password').value;
         const userData = JSON.parse(localStorage.getItem('users'));
         const user = userData.find(user => user.customerId === customerIdE)
-        if(user)
-            {  
-                const custID = user.customerId;
-                const password1 = user.password;
-                if(password === password1)
-                    {
-                    window.location.href = 'index3.html?customerIDURL=' +  custID;
+        if (user) {
+            const custID = user.customerId;
+            const password1 = user.password;
+            if (password === password1) {
+                window.location.href = 'index3.html?customerIDURL=' + custID;
 
-                    window.location.href = 'index3.html?CsId=' +  custID;
-
-                    }
-                else
-                {
-                    alert('Icorrect Password');
-                }
             }
-        else
-        {
+            else {
+                alert('Icorrect Password');
+            }
+        }
+        else {
             console.log("User with the specified customerId not found.");
         }
 
+
         document.getElementById('CustID').value = '';
         document.getElementById('password').value = '';
-        
-    });
-    
 
+    });
+
+    const regBtn = document.getElementById('regBtn');
+    regBtn.addEventListener('click', function () {
+        window.location.href = "index.html";
+    });
 })
 
 
